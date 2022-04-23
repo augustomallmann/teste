a367c4c422fb74dd7dc644abf738344144c364a6
Augusto Mallmann <augustocmallmann@gmail.com>
2022-04-23 16:16:30 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/* @flow*/
+
+import React from "react";
+import { CircleIconButton, Title } from "playbook-ui";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CircleIconButton icon="plus" variant="primary" />
+      <Title text="Welcome to Playbook" />
+      <Title text="Welcome to Pslaybook" />
+    </div>
+  );
+}
